052a527d7988c46b2850945f05cff2b4765a13c9
jualshe <shevchenko.j@gmail.com>
2022-04-21 20:30:55 -0700

    initial commit

---
+++ askNameWithInputs.py
@@ -0,0 +1,13 @@
+# this says hello asks the name and the age
+
+print('yoyo')
+print('what is your name?')  # ask for the name
+myName = input()
+
+print('it is good to meet you, ' + myName)
+print('the lenght of your name is:')
+print(len(myName))
+
+print('What is ysou age?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
