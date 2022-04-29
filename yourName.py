bd2062f22448977813eeb383269204f86bd5ee45
jualshe <shevchenko.j@gmail.com>
2022-04-28 22:56:53 -0700

    update

--- yourName.py
+++ yourName.py
@@ -1,5 +1,20 @@
-name = ''
-while name != 'your name':
-    print('please type your name.')
-    name = input()
-print('thank you!')
+# name = ''
+# while name != 'your name':
+#     print('please type your name.')
+#     name = input()
+# print('thank you!')
+
+# name = ''
+# while True:
+#     print('please type your name.')
+#     name = input()
+#     if name == 'your name':
+#         break
+# print('thank you!')
+
+spam = 0
+while spam < 5:
+    spam = spam + 1
+    if spam == 3:
+        continue
+    print('spam is ' + str(spam))
