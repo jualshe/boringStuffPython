d60e09e4b19f87cc91edf2e290d3e84f70fbfb69
jualshe <shevchenko.j@gmail.com>
2022-04-29 20:59:33 -0700

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,31 @@
+print('enter the name')
+name = input()
+if name:
+    print('hi ' + name)
+else:
+    print('you did not enter the name')
+
+# elif statements
+# name = 'Bob'
+# age = 3000
+# if name == 'Alice':
+#     print('hi, Alice')
+# elif age < 12:
+#     print('you are not Alice, kiddo')
+# elif age > 2000:
+#     print('Alice is not an undead immortal vampire')
+# elif age > 100:
+#     print('you are not Alice, granny')
+
+# -----if else statement
+# password = 'swordfish'
+# if password == 'swordfish':
+#     print('Acces granted.')
+# else:
+#     print('Wrong password.')
+
+# ----- if statement
+# name = 'Bob'
+# if name == 'Alice':
+#     print('Hi Alice')
+# print('Done')
