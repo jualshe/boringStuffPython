5f89220dbfed1464a1b07ca762ea51b22f9129bb
jualshe <shevchenko.j@gmail.com>
2022-05-03 23:39:21 -0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+# def hello():
+#     print('hi')
+#     print('hellow there!')
+#     print('howdy!')
+# hello()
+# hello()
+# hello()
+
+def hello(name):
+    print('hi ' + name)
+
+
+hello('Alice')
+hello('BBobb')
