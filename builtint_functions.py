d218fce905caa475c718027611631f25b4f31da1
jualshe <shevchenko.j@gmail.com>
2022-05-03 18:01:25 -0700

    Update builtint_functions.py

--- builtint_functions.py
+++ builtint_functions.py
@@ -4,5 +4,12 @@ print('hi')
 sys.exit()
 print('goodbuy')
 
+# import random
+#
+# random.randint(1, 10)
+# random.randint(1, 10)
+# random.randint(1, 10)
+# random.randint(1, 10)
+# random.randint(1, 10)
 # pyperclip.copy('hello worldik!')
 # pyperclip.paste()
