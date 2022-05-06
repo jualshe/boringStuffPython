5fc9272744e175b2ccc1b74566d6cf487e99a245
jualshe <shevchenko.j@gmail.com>
2022-05-05 21:31:23 -0700

    Create tryExcept.py

---
+++ tryExcept.py
@@ -0,0 +1,12 @@
+# handle errors with try and except Statement
+def div42by(divideBy):
+    try:
+        return 42 / divideBy
+    except ZeroDivisionError:
+        print('Error: you tried to divide yb zero')
+
+
+print(div42by(12))
+print(div42by(2))
+print(div42by(0))
+print(div42by(1))
