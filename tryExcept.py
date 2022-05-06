50f92f7d00048ac016cd421e6e5c7e062188d554
jualshe <shevchenko.j@gmail.com>
2022-05-05 21:37:25 -0700

    Update tryExcept.py

--- tryExcept.py
+++ tryExcept.py
@@ -1,12 +1,23 @@
-# handle errors with try and except Statement
-def div42by(divideBy):
-    try:
-        return 42 / divideBy
-    except ZeroDivisionError:
-        print('Error: you tried to divide yb zero')
+print('how many cats do you have?')
+numCats = input()
+# because input returns a string value - convert to int
+try:
+    if int(numCats) >= 4:
+        print('that is a lot of cats!')
+    else:
+        print('That is not that many cats')
+except ValueError:
+    print('you did not enter a number')
 
-
-print(div42by(12))
-print(div42by(2))
-print(div42by(0))
-print(div42by(1))
+# # handle errors with try and except Statement
+# def div42by(divideBy):
+#     try:
+#         return 42 / divideBy
+#     except ZeroDivisionError:
+#         print('Error: you tried to divide yb zero')
+#
+#
+# print(div42by(12))
+# print(div42by(2))
+# print(div42by(0))
+# print(div42by(1))
