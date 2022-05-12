43dd5ea501e4f2d2970061f685d123d4fc32faa8
jualshe <shevchenko.j@gmail.com>
2022-05-11 23:39:29 -0700

    Update list.py

--- list.py
+++ list.py
@@ -1,3 +1,28 @@
-supplies = ['pens', 'staples', 'flame thrower', 'binders']
-for i in range(len(supplies)):
-    print('index ' + str(i) + ' in supplies is: ' + supplies[i])
+# cat = ['fat', 'orange', 'loud']
+# size = cat[0]
+# color = cat[1]
+# disposition = cat[2]
+# or the same assignment bby doing -
+# size, color, disposition = cat
+# also the multiple assignment can look like this
+# size, color, disposition = 'skinny', 'black', 'quiet'
+# print(size)
+
+# supplies = ['pens', 'staples', 'flame thrower', 'binders', 'pens', 'staples', 'flame thrower', 'binders', 'pens',
+#             'staples', 'flame thrower', 'binders', 'pens', 'staples', 'flame thrower', 'binders']
+# for i in range(len(supplies)):
+#     print('index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+
+# swapping variables
+# a = 'aaa'
+# b = 'bbb'
+#
+# a, b = b, a
+# print(a)
+
+# augmented assigned operators
+spam = 42
+# spam = spam + 1
+spam += 1
+print(spam)
