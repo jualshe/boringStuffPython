e03a806aa9b2f321deb3e027498cedae803d62f7
jualshe <shevchenko.j@gmail.com>
2022-05-16 23:12:18 -0700

    Update dictionary_data_type.py

--- dictionary_data_type.py
+++ dictionary_data_type.py
@@ -1,3 +1,12 @@
-myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
-print(myCat['size'])
-print('My cat has' + myCat['color'] + ' fur.')
+# myCat = {'size': 'fat', 'color': 'gray', 'disposition': 'loud'}
+# print(myCat['size'])
+# print('My cat has' + myCat['color'] + ' fur.')
+
+# Dictionaries vs. Lists
+spam = ['cats', 'dogs', 'moose']
+bacon = ['dogs', 'moose', 'cats']
+print(spam == bacon)
+
+# eggs = {'name': 'Zophie', 'species': 'cat', 'age': '8'}
+# ham = {'species': 'cat', 'age': '8', 'name': 'Zophie'}
+# print(eggs == ham)
