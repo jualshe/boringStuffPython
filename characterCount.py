614c785684b4e6db921a7efbc75af2b0bb3d41de
jualshe <shevchenko.j@gmail.com>
2022-05-22 09:55:24 -0700

    Update characterCount.py

---
+++ characterCount.py
@@ -0,0 +1,15 @@
+import pprint
+
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+
+for character in message.upper():
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)
+
+# pretty = pprint.pformat(count)
+# print(pretty)
+
+
