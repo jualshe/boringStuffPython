41ce9a460cea659b5d70d342e092f0f7e7f9b28d
jualshe <shevchenko.j@gmail.com>
2022-05-23 22:48:54 -0700

    Update dataStructure.py

---
+++ dataStructure.py
@@ -0,0 +1,9 @@
+cat = {'name': 'Rizhik', 'age': 7, 'color': 'gray'}
+print(cat)
+
+allCats = []
+allCats.append({'name': 'Rizhik', 'age': 7, 'color': 'ginger'})
+allCats.append({'name': 'Chernushka', 'age': 5, 'color': 'bblack'})
+allCats.append({'name': 'Coco', 'age': 5, 'color': 'gray'})
+allCats.append({'name': '???', 'age': -1, 'color': 'orange'})
+print(allCats)
