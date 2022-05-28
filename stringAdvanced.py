99e53b6df1f44b9ed769afcc355ff2d060843cbe
jualshe <shevchenko.j@gmail.com>
2022-05-27 17:45:41 -0700

    Update stringAdvanced.py

--- stringAdvanced.py
+++ stringAdvanced.py
@@ -3,3 +3,18 @@ print("That's her cat")
 
 # backslash before the single quote
 print('That\'s a Bob\'s mother')
+
+print('\nHello!\n\tHow are you?\nI\'m fine')
+
+print(r'That is Carol\'s cat')
+
+print("""Dear Bob,
+your cat has been arrested.
+Sincerely,
+Nate""")
+
+spam = """Dear Bob,
+your cat has been arrested.
+Sincerely,
+Nate"""
+print(spam)
