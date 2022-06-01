93b274a1c709e1a902ef286958dd559945e5f46a
jualshe <shevchenko.j@gmail.com>
2022-05-31 23:00:06 -0700

    Update stringMethods.py

---
+++ stringMethods.py
@@ -0,0 +1,14 @@
+# spam = 'Hello world!'
+# print(spam.upper())
+# print(spam)
+#
+# spam = spam.upper()
+# print(spam)
+# print('\n\n' + spam.lower())
+
+print('Enter your answer')
+answer = input()
+print(answer)
+# answer.lower() == 'yes'
+if answer.lower() == 'yes':
+    print('Playing again')
