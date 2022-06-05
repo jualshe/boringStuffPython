f864c7be4b93db428921401025c777691cf08322
jualshe <shevchenko.j@gmail.com>
2022-06-04 23:57:03 -0700

    Update stringMethods.py

--- stringMethods.py
+++ stringMethods.py
@@ -15,10 +15,14 @@ print(spam)
 
 spam = 'hello world!'
 print(spam.isupper())
-print(spam.islower())
+# print(spam.islower())
 
 print('This Is Title Case'.istitle())
 
 print('Hi!'.startswith('H'))
 
 print(','.join(['cats', 'llamas','copybaras']))
+ko='JULI'
+print(' kaka\n'.join(ko))
+
+print('My name is Juju'.split())
