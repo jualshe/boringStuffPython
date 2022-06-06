da1cd12ba237f5b197052b4c8565658898c18c53
jualshe <shevchenko.j@gmail.com>
2022-06-06 13:29:44 -0700

    Update stringFormatting.py

---
+++ stringFormatting.py
@@ -0,0 +1,12 @@
+print('hello' + 'world')
+
+name = 'Alex'
+place = 'Main street'
+time = '6 pm'
+food = 'bananas'
+
+print('Hello ' + name + ', you are invited to a party at ' + place +
+      ' at ' + time + '. Please bring ' + food )
+
+print(('Hello %s, you are invited to a party at %s at %s.'
+       ' Please bring %s' % (name, place, time, food)))
