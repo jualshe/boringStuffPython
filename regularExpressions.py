c1cdbb03dc5fa8e499058d3da394cb6e77ca5c38
jualshe <shevchenko.j@gmail.com>
2022-06-12 23:56:09 -0700

    Update regularExpressions.py

--- regularExpressions.py
+++ regularExpressions.py
@@ -16,4 +16,13 @@ def isPhoneNumber(text):
             return False #missing last 4 digits
     return True
 
-print(isPhoneNumber('650-440-0172'))
+# print(isPhoneNumber('650-440-0172'))
+message = 'Call me 650-440-0172 tomorrow, or 650-440-0172 '
+foundNumbber = False
+for i in range(len(message)):
+    chunk = message[i:i+12]
+    if isPhoneNumber(chunk):
+        print('Phone number found ' +chunk )
+        foundNumbber = True
+if not foundNumbber:
+    print('Could not find any number')
