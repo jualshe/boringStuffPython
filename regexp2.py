0f793144fa6de023d2a41c4c180cd98c79b658d7
jualshe <shevchenko.j@gmail.com>
2022-06-14 23:55:39 -0700

    Update regexp2.py

--- regexp2.py
+++ regexp2.py
@@ -1,10 +1,12 @@
 import re
 message = 'Call me 650-440-0172 tomorrow, or 650-440-0172 '
-foundNumbber = False
-for i in range(len(message)):
-    chunk = message[i:i+12]
-    if isPhoneNumber(chunk):
-        print('Phone number found ' +chunk )
-        foundNumbber = True
-if not foundNumbber:
-    print('Could not find any number')
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+
+# foundNumbber = False
+# for i in range(len(message)):
+#     chunk = message[i:i+12]
+#     if isPhoneNumber(chunk):
+#         print('Phone number found ' +chunk )
+#         foundNumbber = True
+# if not foundNumbber:
+#     print('Could not find any number')
