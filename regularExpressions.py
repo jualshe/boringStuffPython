8f5af8b4e6a89a0f7fb3f3eb1d7c42033e02a75f
jualshe <shevchenko.j@gmail.com>
2022-06-15 23:38:17 -0700

    Update regularExpressions.py

--- regularExpressions.py
+++ regularExpressions.py
@@ -1,22 +1,23 @@
 def isPhoneNumber(text):
     if len(text) != 12:
-        return False #not phone number sized
+        return False # not phone number sized
     for i in range (0,3):
         if not text[i].isdecimal():
             return False #no area code
     if text[3] != '-':
-        return False #missing dash
+        return False # missing dash
     for i in range (4,7):
         if not text[i].isdecimal():
-            return False #nofirst 3 digits
+            return False # no first 3 digits
     if text[7] != '-':
-        return False  # missing 2nd dash
+        return False  # missing second dash
     for i in range (8,12):
         if not text[i].isdecimal():
-            return False #missing last 4 digits
+            return False # missing last 4 digits
     return True
 
 # print(isPhoneNumber('650-440-0172'))
+
 message = 'Call me 650-440-0172 tomorrow, or 650-440-0172 '
 foundNumbber = False
 for i in range(len(message)):
