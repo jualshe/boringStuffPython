70159242eddbec237fd368296d3b637d525c8756
jualshe <shevchenko.j@gmail.com>
2022-06-16 23:57:05 -0700

    Update regexGroup.py

--- regexGroup.py
+++ regexGroup.py
@@ -8,3 +8,12 @@ mo = phoneNumRegex.search('My number is 650-555-4242')
 print(mo.group())
 print(mo.group(1))
 print(mo.group(2))
+
+phoneNumRegex = re.compile(r'\(\d\d\d\)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is (650)-555-4242')
+print(mo.group())
+
+#pipes
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+mo = batRegex.search('Batmobile lost a wheel')
+print(mo.group())
