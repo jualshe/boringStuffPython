9709c92bc06aae48ab5110f2d969ee08e1c54c2c
jualshe <shevchenko.j@gmail.com>
2022-06-16 23:52:11 -0700

    Update regexGroup.py

---
+++ regexGroup.py
@@ -0,0 +1,10 @@
+import re
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search('My number is 650-555-4242')
+print(mo.group())
+
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search('My number is 650-555-4242')
+print(mo.group())
+print(mo.group(1))
+print(mo.group(2))
