fd1b58466a589ae004d47f7f366eb17a605d8b50
jualshe <shevchenko.j@gmail.com>
2022-06-19 23:17:13 -0700

    Update repetitionRegex.py

--- repetitionRegex.py
+++ repetitionRegex.py
@@ -4,3 +4,9 @@ batRegex = re.compile(r'Bat(wo)?man')
 
 mo = batRegex.search('The adventures of Batman')
 print(mo.group())
+
+mo = batRegex.search('The adventures of Batwoman')
+print(mo.group())
+
+mo = batRegex.search('The adventures of Batwowowowoman')
+mo = None #True
