f8666e51003a2858298a3e78b078564a1924971c
jualshe <shevchenko.j@gmail.com>
2022-06-24 20:47:09 -0700

    Update regexFinaAll.py

--- regexFinaAll.py
+++ regexFinaAll.py
@@ -7,3 +7,7 @@ print(mo.group())
 
 mo = phoneRegex.findall('hi there aloha 540-333-2222 and 111-222-3456')
 print(mo)
+
+phoneRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneRegex.findall('hi there aloha 540-333-2222 and 111-222-3456')
+print(mo)
