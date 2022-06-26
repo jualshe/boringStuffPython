75d95a76992b0047a2023aa83e704f1ce620ae7d
jualshe <shevchenko.j@gmail.com>
2022-06-26 14:15:45 -0700

    Update regexCharClasses.py

--- regexCharClasses.py
+++ regexCharClasses.py
@@ -1,9 +1,11 @@
 import re
-
+#\d numeric from 0 to 9
 digitRegex = re.compile(r'(0|1|2|3|4|5|6|7|8|9|)')
 #same as
 digitRegex = re.compile(r'\d')
 
 # \D any character that is not a numeric digit from 0 to 9
-# \w any letter, numeric, or undescor character  'word'
-# \W any numeric that is not a letter, numeric or undescore
+# \w any letter, numeric, or undescore character  'word'
+# \W any numeric that is not a letter, numeric or undescoree
+# \s any space, tab or new line character
+# \S any character that is not a space, tab or new line
