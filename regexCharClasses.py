4199ff5aeecbfb93d3cd8ff10260717d4a7302bf
jualshe <shevchenko.j@gmail.com>
2022-06-26 14:23:24 -0700

    Update regexCharClasses.py

--- regexCharClasses.py
+++ regexCharClasses.py
@@ -9,3 +9,10 @@ digitRegex = re.compile(r'\d')
 # \W any numeric that is not a letter, numeric or undescoree
 # \s any space, tab or new line character
 # \S any character that is not a space, tab or new line
+
+
+lyrics = '12 drummers, 11 pipers, 10 lords, 9 ladies, 8 maids, 7 swans'
+
+xmasRegex = re.compile(r'\d+\s\w+')
+mo = xmasRegex.findall(lyrics)
+print(mo)
