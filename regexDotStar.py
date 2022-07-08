e728d92d0f59c2f1b193f8d82c6a47e9c0380914
jualshe <shevchenko.j@gmail.com>
2022-07-07 21:41:14 -0700

    Update regexDotStar.py

--- regexDotStar.py
+++ regexDotStar.py
@@ -49,3 +49,26 @@ print(mo)
 greedy = re.compile(r'<(.*)>')
 mo = greedy.findall(serve)
 print(mo)
+
+prime = '\nServe the pubblic trust. \nProtect the innocent. \nUpload the law.\n'
+print(prime)
+
+# greedy doesn't include the new line
+dotStar = re.compile(r'.*')
+mo = dotStar.search(prime)
+print(mo)
+
+# the way to include everything
+dotStar = re.compile(r'.*', re.DOTALL)
+mo = dotStar.search(prime)
+print(mo)
+
+# case insensitive regular expression match
+
+vovelRegex = re.compile(r'[aeiou]')
+mo = vovelRegex.findall('Al, why your programming book talks about Robbocop so much')
+print(mo)
+
+vovelRegex = re.compile(r'[aeiou]', re.IGNORECASE)  # re.IGNORECASE = re.I
+mo = vovelRegex.findall('Al, why your programming book talks about Robbocop so much')
+print(mo)
