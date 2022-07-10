f4500fb73273c87201a0c6f3e094466e1c8ef835
jualshe <shevchenko.j@gmail.com>
2022-07-09 23:27:23 -0700

    Update regexSubMethod.py

--- regexSubMethod.py
+++ regexSubMethod.py
@@ -1,3 +1,7 @@
 import re
 
 namesRegex = re.compile(r'Agent \w+')
+text = 'Agent Alice gave the secret documents to Agent Bob.'
+mo = namesRegex.findall(text)
+print(mo)
+print(namesRegex.sub('REDACTED', text))
