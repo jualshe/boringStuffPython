02787bc67a92476e2d5574676ada73aa89bc4a17
jualshe <shevchenko.j@gmail.com>
2022-07-09 23:32:20 -0700

    Update regexSubMethod.py

--- regexSubMethod.py
+++ regexSubMethod.py
@@ -1,7 +1,16 @@
 import re
 
 namesRegex = re.compile(r'Agent \w+')
-text = 'Agent Alice gave the secret documents to Agent Bob.'
+text = 'Agent Alice gave the secret documents to Agent Bob and Agent C.'
 mo = namesRegex.findall(text)
 print(mo)
 print(namesRegex.sub('REDACTED', text))
+
+# using \1, \2 etc in sub()
+namesRegex = re.compile(r'Agent (\w)\w*')
+text = 'Agent Alice gave the secret documents to Agent Bob.'
+mo = namesRegex.findall(text)
+print(mo)
+
+mo = namesRegex.sub(r'Agent \1****', text)
+print(mo)
