a96c77a3c25738e0caf55815a9f34363687e1c29
jualshe <shevchenko.j@gmail.com>
2022-07-13 23:56:58 -0700

    Update regexVerboseMode.py

--- regexVerboseMode.py
+++ regexVerboseMode.py
@@ -1,8 +1,11 @@
 import re
 
 re.compile(r'''
-\d\d\d      #area code
--           #1st dash
+(\d\d\d-)|      #area code (without parens)
+(\(\d\d\d\) )           # - or- area code with parens and no dash
 \d\d\d      #first 3 digits
 -           #second dash
-\d\d\d\d    #last 4 digits''', re.VERBOSE)
+\d\d\d\d    #last 4 digits
+\sx\d{2,4} #extension, like x1234''', re.VERBOSE | re.IGNORECASE | re.DOTALL)
+
+# re.IGNORECASE (I)
