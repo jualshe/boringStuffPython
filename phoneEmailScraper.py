9f72454c3c8eb3a0357c0854222e31cafaf5789f
jualshe <shevchenko.j@gmail.com>
2022-07-16 23:52:55 -0700

    Update phoneEmailScraper.py

--- phoneEmailScraper.py
+++ phoneEmailScraper.py
@@ -5,11 +5,11 @@ import re, pyperclip
 re.compile(r'''
 #415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext 12345, x12345
 
-((\d\d\d) | (\(\d\d\d))?  # area code (optional)
-        # firts separator
-        # first 3 digits
-        # separator 
-        # last 4 digits
+((\d\d\d) | (\(\d\d\d\)))?  # area code (optional)
+(\s|-)        # firts separator
+\d\d\d        # first 3 digits
+-        # separator 
+\d\d\d\d        # last 4 digits
         #extension (optional)
 
 ''', re.VERBOSE)
