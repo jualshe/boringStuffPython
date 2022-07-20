a44e578c06bcf99c55d723ae3db7b2759fea81f3
jualshe <shevchenko.j@gmail.com>
2022-07-20 00:54:22 -0700

    Update phoneEmailScraper.py

--- phoneEmailScraper.py
+++ phoneEmailScraper.py
@@ -6,10 +6,12 @@ import pyperclip
 phoneRegex = re.compile(r'''
 #415-555-0000, 555-0000, (415) 555-0000, 555-0000 ext 12345, ext 12345, x12345
 (
-((\d\d\d) | (\(\d\d\d\)))?  # area code (optional)
-(\s|-)        # firts separator
+[+\d]?      #country code
+(\s|-|.)?          #separator between country and a number
+((\d\d\d) | (\d\d\d\d) | (\(\d\d\d\)))?  # area code (optional)
+(\s|-|.)?        # firts separator
 \d\d\d        # first 3 digits
--        # separator 
+(\s|-|.)?       # separator 
 \d\d\d\d        # last 4 digits
 (((ext(\.)?\s)|x)    #extension (optional)
 (\d{2,5}))? )       #extension number-part (optional)
@@ -21,10 +23,9 @@ phoneRegex = re.compile(r'''
 # TODO: create a regex for email addresses
 emailRegex = re.compile(r'''
 # some.+_thing@something.com
-(
 [a-zA-Z0-9_.+]+    #name part
 @                  # @symbol
-[a-zA-Z0-9_.+]+ )   #domain part
+[a-zA-Z0-9_.+]+   #domain part
 ''', re.VERBOSE)
 
 # Get the text off the clipbboard
