40b855bb3954ccce3847f8b01ed5f7ed5bd1b69a
jualshe <shevchenko.j@gmail.com>
2022-07-22 00:16:39 -0700

    Update filenames.py

--- filenames.py
+++ filenames.py
@@ -7,3 +7,19 @@ print(a)
 
 print(os.path.join('folder1', 'folder2', 'file.png'))
 print(os.getcwd())
+print(os.path.abspath('beach.jpg'))
+print(os.path.abspath('../../beach.jpg'))
+
+print(os.path.isabs('/Users/julia/dev/beach.jpg'))
+print(os.path.isabs('../../beach.jpg'))
+
+print(os.path.relpath('/Users/julia/dev/images/beach.jpg', '/Users/julia'))
+
+print(os.path.dirname('/Users/julia/dev/images/beach.jpg'))
+print(os.path.basename('/Users/julia/dev/images/beach.jpg'))
+print(os.path.basename('/Users/julia/dev/images'))
+
+print(os.path.exists('/Users/julia/dev/images/folderN/beach.jpg'))
+
+print(os.path.isfile('/Users/julia/dev/images/beach.jpg'))
+print(os.path.isdir('/Users/julia/dev/images/beach.jpg'))
