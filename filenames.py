f38e3132a21a4de214e421ad1d92210df62518f0
jualshe <shevchenko.j@gmail.com>
2022-07-21 23:47:45 -0700

    Update filenames.py

---
+++ filenames.py
@@ -0,0 +1,9 @@
+# using strings to represent a filepath
+import os
+
+a = '/Users/julia/dev/images/beach.jpg'
+
+print(a)
+
+print(os.path.join('folder1', 'folder2', 'file.png'))
+print(os.getcwd())
