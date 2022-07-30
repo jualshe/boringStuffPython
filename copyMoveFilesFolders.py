4119a5f41796c70723bc9380b117c1d59983fbba
jualshe <shevchenko.j@gmail.com>
2022-07-30 12:00:25 -0700

    Update copyMoveFilesFolders.py

---
+++ copyMoveFilesFolders.py
@@ -0,0 +1,7 @@
+import shutil
+
+shutil.copy('/Users/julia/Desktop/sofa1.png', '/Users/julia/Downloads/sofasofa.png')
+# shutil.copytree('/Users/julia/Desktop/delicious', '/Users/julia/Downloads/delicious_backup')
+# shutil.move('/Users/julia/Desktop/delicious/sofa2.png', '/Users/julia/Downloads/delicious_backup/ololo')
+shutil.move('/Users/julia/Downloads/delicious_backup/ololo/sofa2.png',
+            '/Users/julia/Downloads/delicious_backup/ololo/kokoko.png')
