8883ab70932de19880f1de1c5d9bb858caa08a52
jualshe <shevchenko.j@gmail.com>
2022-08-02 23:29:56 -0700

    Update deleteFilesFolders.py

--- deleteFilesFolders.py
+++ deleteFilesFolders.py
@@ -1,9 +1,11 @@
 import os, shutil
 import send2trash
 
+# delete a file -> move to trash
 # send2trash.send2trash('/Users/julia/Desktop/sofa1.png')
 
 os.getcwd()
+# delete a file
 # os.unlink('')
 
 # delete empty folder
@@ -12,8 +14,9 @@ os.getcwd()
 # delete folder and content
 # shutil.rmtree('')
 
+# to prevent deleting try to run print first and then unlink
 os.chdir('/Users/julia/Desktop')
 for filename in os.listdir():
     if filename.endswith('.txt'):
         print(filename)
-        os.unlink(filename)
+        send2trash.send2trash(filename)
