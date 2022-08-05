72475d2036a31a3f33329ef95766315f3c539585
jualshe <shevchenko.j@gmail.com>
2022-08-04 23:48:18 -0700

    Update walkingDirectoryTree.py

--- walkingDirectoryTree.py
+++ walkingDirectoryTree.py
@@ -1,7 +1,18 @@
 import os
+import shutil
 
-for folderName, subfolder, filenames in os.walk('/Users/julia/Desktop/delicious'):
+for folderName, subfolders, filenames in os.walk('/Users/julia/Desktop/delicious'):
     print('The folder is ' + folderName)
-    print('The subfolders in ' + folderName + 'are: ' + str(subfolder))
+    print('The subfolders in ' + folderName + 'are: ' + str(subfolders))
     print('The filenames in ' + folderName + 'are: ' + str(filenames))
     print()
+
+for subfolder in subfolders:
+    print(subfolder)
+    # if 'fish' in subfolder:
+    #     os.rmdir(subfolder)
+    #     os.unlink(subfolder)
+
+for file in filenames:
+    if file.endswith('.py'):
+        shutil.copy(os.join(folderName, file), os.join(folderName, file + '.backup'))
