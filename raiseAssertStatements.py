0d400380386bd0cb5bdb0723b442433653bc343c
jualshe <shevchenko.j@gmail.com>
2022-08-09 00:10:02 -0700

    Update raiseAssertStatements.py

--- raiseAssertStatements.py
+++ raiseAssertStatements.py
@@ -14,6 +14,8 @@ def boxPrint(symbol, width, height):
     if len(symbol) != 1:
         raise Exception('"symbol" needs to a string of lenght 1')
     print(symbol * width)
+    if (width < 2) or (height < 2):
+        raise Exception('width and height must be greater or equal than 2')
 
     for i in range(height - 2):
         print(symbol + (' ' * (width - 2)) + symbol)
@@ -22,5 +24,7 @@ def boxPrint(symbol, width, height):
 
 
 boxPrint('*', 15, 5)
+boxPrint('~', 25, 5)
+boxPrint('~', 25, 1)
 # raise Exception('This is the error message.')
 boxPrint('**', 15, 5)
