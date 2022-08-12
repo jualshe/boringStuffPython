047f938257a0a38fb3b67a1ae801b9e0f78dc029
jualshe <shevchenko.j@gmail.com>
2022-08-12 16:46:13 -0700

    Update logging.py

---
+++ logging.py
@@ -0,0 +1,12 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(messages)s')
+
+
+def factorial(n):
+    for i in range(n + 1):
+        total *= i
+        return total
+
+
+print(factorial(5))
