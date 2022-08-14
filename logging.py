00d4c8c49e2b2b819a28afa57eb7581063781799
jualshe <shevchenko.j@gmail.com>
2022-08-13 23:23:05 -0700

    Update logging.py

--- logging.py
+++ logging.py
@@ -2,11 +2,17 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(messages)s')
 
+logging.debug('Start of the program')
+
 
 def factorial(n):
+    logging.debug('start of factorial(%s)' % (n))
+    total = 1
     for i in range(n + 1):
         total *= i
-        return total
+        logging.debug('i is %s, total is %s' % (i, total))
+    logging.debubg('return value is %s' % (total))
+    return total
 
 
 print(factorial(5))
