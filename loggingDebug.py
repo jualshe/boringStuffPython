6162c3df56adf07cb5843adddc969d474098a5db
jualshe <shevchenko.j@gmail.com>
2022-08-17 00:21:26 -0700

    Update loggingDebug.py

--- loggingDebug.py
+++ loggingDebug.py
@@ -1,21 +1,20 @@
 import logging
 
-# import sys
-#
-# print(sys.path)
-#
-# print(logging.__file__)
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(messages)s')
+# write log to a file
+logging.basicConfig(filename='testLogDebug  .txt', format='%(asctime)s - %(levelname)s - %(messages)s')
+# logging.disable(logging.CRITICAL)
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(messages)s')sla
 logging.debug('Start of the program')
 
 
 def factorial(n):
     logging.debug('start of factorial(%s)' % (n))
     total = 1
-    for i in range(n + 1):
+    for i in range(1, n + 1):
         total *= i
         logging.debug('i is %s, total is %s' % (i, total))
+
     logging.debug('return value is %s' % (total))
     return total
 
