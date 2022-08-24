9bddc49d28e93cf27f65dfd114df4004ed77878b
jualshe <shevchenko.j@gmail.com>
2022-08-23 18:23:13 -0700

    Update webbrowsingModule.py

--- webbrowsingModule.py
+++ webbrowsingModule.py
@@ -1,1 +1,14 @@
-import webbrowser, sys
+import webbrowser, sys, pyperclip
+
+sys.argv  # ['webbrowsingModule.py', '870', 'Valencia', 'St.']
+# check if command line arguments were passed
+
+
+if len(sys.argv) > 1:
+    # ['webbrowsingModule.py', '870', 'Valencia', 'St.'] - > '870 Valencia St.'
+    address = ''.join(sys.argv[1:])
+else:
+    address = pyperclip.paste()
+
+# https://www.google.com/maps/place/<ADDRESS>
+webbrowser.open('https://www.google.com/maps/place/' + address)
