a661f5d8a043b56006c0adbeba115f0d89cbd295
jualshe <shevchenko.j@gmail.com>
2022-08-26 23:58:47 -0700

    Update webDownload.py

--- webDownload.py
+++ webDownload.py
@@ -1,3 +1,11 @@
 import requests
 
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(res.status_code)
+
+print(len(res.text))
+print(res.text[:50])
+
+badRes = requests.get('https://automatetheboringstuff.com/files/rj.1111txt')
+
+print(badRes.raise_for_status())
