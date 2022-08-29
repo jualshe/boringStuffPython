925cc5e5db9d1ffd5a235776eba8478b8321b643
jualshe <shevchenko.j@gmail.com>
2022-08-28 23:53:41 -0700

    Update parsingHTMLBeautifulSoupModule.py

--- parsingHTMLBeautifulSoupModule.py
+++ parsingHTMLBeautifulSoupModule.py
@@ -3,7 +3,7 @@ import bs4
 import requests
 
 res = requests.get('https://www.amazon.com/Automate-Boring-Stuff-Python-2nd/dp/1593279922/')
-# res.raise_for_status()
+print(res.raise_for_status())
 soup = bs4.BeautifulSoup(res.text)
 
 print(soup)
