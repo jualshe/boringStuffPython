8ca82c36111d46b2d670698a861bbf8c120c7723
jualshe <shevchenko.j@gmail.com>
2022-09-05 23:18:33 -0700

    Update webDownload.py

--- webDownload.py
+++ webDownload.py
@@ -1,14 +1,15 @@
 import requests
 
 res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
-print(res.status_code)
+print('Status code ' + str(res.status_code))
 
 print(len(res.text))
-print(res.text[:50])
+print(res.text[:500])
 
+# raise the error if present and do nothing if succeeded
 print(res.raise_for_status())
 
-# handle the error case without crahsin
+# handle the error case without crashing
 # badRes = requests.get('https://automatetheboringstuff.com/files/rj.1111txt')
 # print(badRes.raise_for_status())
 
@@ -16,3 +17,5 @@ playFile = open('RomeoAndJuliet.txt', 'wb')
 for chunk in res.iter_content(100000):
     playFile.write(chunk)
 playFile.close()
+
+print(playFile)
