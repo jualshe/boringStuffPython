f1cc0b7827544a08d0bd5c416c84ae4c316d9770
jualshe <shevchenko.j@gmail.com>
2022-09-06 22:40:15 -0700

    Update parsingHTMLBeautifulSoupModule.py

--- parsingHTMLBeautifulSoupModule.py
+++ parsingHTMLBeautifulSoupModule.py
@@ -1,13 +1,16 @@
 # web scraping
 import bs4, requests
 
-headers = {'User-Agent': 'Mozilla/5.0'}
 
-res = requests.get('https://www.amazon.com/Automate-Boring-Stuff-Python-2nd/dp/1593279922/')
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
+def getAmazonPrice(productURL):
+    res = requests.get(productURL, headers={
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36"})
+    res.raise_for_status()
 
-# elems = soup.select('#corePrice_feature_div > div > span > span:nth-child(2)')
-elems = soup.select('#listPrice')
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    elems = soup.select('#usedPrice')
+    return elems[0].text.strip()
 
-print(elems)
+
+price = getAmazonPrice('https://www.amazon.com/Automate-Boring-Stuff-Python-2nd/dp/1593279922')
+print('The price is ' + price)
