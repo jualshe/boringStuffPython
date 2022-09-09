e7597d5b42abe71d3b5b7702345bd258eef2b7ff
jualshe <shevchenko.j@gmail.com>
2022-09-09 00:08:09 -0700

    Update seleniumModule.py

--- seleniumModule.py
+++ seleniumModule.py
@@ -4,5 +4,15 @@ from selenium.webdriver.common.by import By
 browser = webdriver.Firefox()
 browser.get('https://automatetheboringstuff.com')
 
+elem = browser.find_element(By.CSS_SELECTOR, '.main > main:nth-child(1) > div:nth-child(1) > p:nth-child(6)')
+print(elem.text)
+
+elem1 = browser.find_element(By.CSS_SELECTOR, 'html')
+print(elem1.text)
+
 link = browser.find_element(By.LINK_TEXT, 'Introduction')
 browser.execute_script("arguments[0].click();", link)
+browser.back()
+browser.forward()
+browser.refresh()
+browser.quit()
