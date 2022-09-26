8ecf0e36a193b4f7e74ee12140cb3739362c5572
jualshe <shevchenko.j@gmail.com>
2022-09-25 23:43:39 -0700

    Update sendingEmails.py

--- sendingEmails.py
+++ sendingEmails.py
@@ -10,8 +10,9 @@ print(connection.ehlo())
 connection.starttls()
 
 # enter email and password
-connection.login('', '')
+connection.login('shevchenkoj@yahoo.com', '')
 # enter email from and email to
-connection.sendmail('', '', 'Subject: test subject \n\n Hi there! \n how are you? \n\n - Julia ')
+connection.sendmail('julia@relola.com', 'shevchenko.j@gmail.com',
+                    'Subject: test subject \n\n Hi there! \n how are you? \n\n - Julia ')
 
 connection.quit()
