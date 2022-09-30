a405913619ee812979d9fc4961b9d50bb289498e
jualshe <shevchenko.j@gmail.com>
2022-09-29 17:27:33 -0700

    Update paintMoveMouse.py

--- paintMoveMouse.py
+++ paintMoveMouse.py
@@ -1,13 +1,19 @@
 # painnt online https://paint.js.org/
-
 import pyautogui
 
 pyautogui.click()
 distance = 200
 while distance > 0:
     print(distance, 0)
-    pyautogui.dragRel(distance, 0, duration=1, 5)  # move right
+    pyautogui.dragRel(distance, 0, duration=0.1, button='left')  # move right
     distance = distance - 25
 
     print(0, distance)
-    pyautogui.dragRel()
+    pyautogui.dragRel(0, distance, duration=0.1, button='left')  # move down
+
+    print(-distance, 0)
+    pyautogui.dragRel(-distance, 0, duration=0.1, button='left')  # move left
+    distance = distance - 25
+
+    print(0, -distance)
+    pyautogui.dragRel(0, -distance, duration=0.1, button='left')  # move up
